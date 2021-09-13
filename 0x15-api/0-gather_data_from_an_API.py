9702d53dc5ceb11d674b0cb633c9525d05a49375
Daniel Mishan <77964799+Ksualboy@users.noreply.github.com>
2021-09-13 16:42:06 -0300

    fixing task print

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -28,4 +28,4 @@ if __name__ == '__main__':
                                                           completed, nTasks))
 
     for task in taskNames:
-        print("\t" + task)
+        print("\t " + task)
