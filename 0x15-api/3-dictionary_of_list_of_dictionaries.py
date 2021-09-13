3ca4c4289fc43b22b2c72e4bb26b2311a3e68d10
Ksualboy <2758@holbertonschool.com>
2021-09-13 20:32:43 +0000

    adding 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,27 +2,23 @@
 ''' Script that uses a REST API '''
 
 if __name__ == '__main__':
-    from sys import argv
     import requests
     import json
 
-    # Getting tasks
-    taskResponse = requests.get('https://jsonplaceholder.typicode.com/todos')
-    taskResponse = json.loads(taskResponse.text)
+    url = "https://jsonplaceholder.typicode.com/"
 
-    # Getting Employee data
-    userResponse = requests.get('https://jsonplaceholder.typicode.com/users',
-                                params={'id': argv[1]})
-    userResponse = json.loads(userResponse.text)
-    id = userResponse[0]["id"]
-    username = userResponse[0]["username"]
+    userResponse = requests.get(url + "users").json()
 
     data = {}
-    data[id] = []
+    for user in userResponse:
+        id = user["id"]
+        username = user["username"]
+        data[id] = []
+        tasks = requests.get(url+"todos",params={"userId": user["id"]}).json()
+        for task in tasks:
+            data[id].append({'task': task["title"],
+                            'completed': task["completed"],
+                            'username': username})
 
-    for task in taskResponse:
-        data[id].append({'task': task["title"], 'completed': task["completed"],
-                         'username': username})
-
-    with open(str(id) + '.json', 'w') as file:
+    with open('todo_all_employees.json', 'w') as file:
         json.dump(data, file)
